24624f30b22abc891c5d1dd4be1549f211132c17
superfhwl <superfhwl@gmail.com>
2024-10-21 18:08:44 +0800

    Add orca gym env type

--- robomimic/utils/env_utils.py
+++ robomimic/utils/env_utils.py
@@ -41,6 +41,9 @@ def get_env_class(env_meta=None, env_type=None, env=None):
     elif env_type == EB.EnvType.IG_MOMART_TYPE:
         from robomimic.envs.env_ig_momart import EnvGibsonMOMART
         return EnvGibsonMOMART
+    elif env_type == EB.EnvType.ORCA_GYM_TYPE:
+        from robomimic.envs.env_orca_gym import EnvOrcaGym
+        return EnvOrcaGym
     raise Exception("code should never reach this point")
 
 
